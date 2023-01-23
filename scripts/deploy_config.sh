8d34719cee8def0c4e5cd53a588a1fb03a802e54
myFavBalu <skeyhl@gmail.com>
2023-01-23 11:05:42 +0100

    added potential php-nginx-conf and fixed deploy_config.sh

--- scripts/deploy_config.sh
+++ scripts/deploy_config.sh
@@ -5,5 +5,9 @@ source config.sh
 echo "${info}copy config to server${default}"
 cd ../
 scp -r serverconfig/nginx what2cook:/etc
+echo "${success}finished copying config!${default}"
 
-echo "${success}finished deploying config!${default}"
+echo "${info} restarting nginx${default}"
+ssh what2cook -c "systemctl restart nginx -vvv"
+
+echo "${success}finished deplyoing config!${default}"
