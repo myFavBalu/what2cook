8256992707ec9b37e4c122b05d410051adfe8d6f
myFavBalu <skeyhl@gmail.com>
2023-05-10 10:52:03 +0200

    added new recipes and a db-deploy-script

---
+++ scripts/deploy_database.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source config.sh
+
+echo "${info}copying database to server${default}"
+
+scp -r ../backend/var/data.db what2cook:/var/www/html/backend/var
+
+echo "${success}finished deploying database!${default}"
