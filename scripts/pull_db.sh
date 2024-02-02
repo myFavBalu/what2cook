7c25bc43e3e0cd93ba51aa8122bafdfee04382f2
myFavBalu <skeyhl@gmail.com>
2024-02-02 11:15:45 +0100

    fixed styling of instructions, cleaned up db, made pull_db more generic and renamed deploy_db

--- scripts/pull_db.sh
+++ scripts/pull_db.sh
@@ -4,6 +4,6 @@ source config.sh
 
 echo "${info}Copying database from server${default}"
 
-scp what2cook:/var/www/html/backend/var/data.db /home/lbrie/IdeaProjects/what2cook/backend/var/data.db
+scp what2cook:/var/www/html/backend/var/data.db ../backend/var/data.db
 
 echo "${success}Successfully copied Db from server${default}"
