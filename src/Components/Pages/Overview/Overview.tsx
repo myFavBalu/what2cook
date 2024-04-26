12f2514071f1112fa1d9804b91dc92b06e6836ff
myFavBalu <skeyhl@gmail.com>
2024-04-26 08:50:40 +0200

    added styling to overview

--- src/Components/Pages/Overview/Overview.tsx
+++ src/Components/Pages/Overview/Overview.tsx
@@ -1,10 +1,13 @@
 import {MealSearchResult} from "../../../Types/MealTypes";
 import React, {useEffect, useState} from "react";
+import "./Overview.scss"
 import {getMealNamesWithIds} from "../../../ApiCalls/getMealNamesWithIds";
+import {useNavigate} from "react-router-dom";
 
 
 export function Overview(): JSX.Element {
     const [mealPlaceholders, setMealPlaceholders] = useState<MealSearchResult[]>([]);
+    const navigation = useNavigate()
 
     useEffect(() => {
         getMealNamesWithIds().then((mealPlaceholders) => {
@@ -12,7 +15,15 @@ export function Overview(): JSX.Element {
         })
     }, [])
 
-    return <div>
-        {mealPlaceholders.map((placeholder) => <div>{placeholder.name}</div>)}
+    if (mealPlaceholders.length === 0) {
+        return <></>;
+    }
+
+    return <div className={"OverviewContainer"}>
+        {mealPlaceholders.map((placeholder) =>
+            <div className={"OverviewElement"} key={placeholder.id} onClick={() => {
+                navigation({pathname: "/generate", search: "?recipeId=" + placeholder.id})
+            }}>{placeholder.name}</div>)
+        }
     </div>;
 }
