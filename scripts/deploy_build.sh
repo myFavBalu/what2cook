f7824f9b74d3b2a2e010e823a9db40ec833b2cba
myFavBalu <skeyhl@gmail.com>
2024-07-30 17:15:57 +0200

    refactored deploy_build to utilize compression updated frontend dependencies

--- scripts/deploy_build.sh
+++ scripts/deploy_build.sh
@@ -6,7 +6,19 @@ echo "${info}building project${default}"
 cd ../
 yarn build
 
+tar -czvf build.tar.gz build
+
 echo "${info}copying build to server${default}"
-scp -r build what2cook:/var/www/html
+scp -r build.tar.gz what2cook:/var/www/html
+
+echo "${info}removing old build${default}"
+ssh what2cook "rm -rf /var/www/html/build"
+
+echo "${info}unpacking new build on server${default}"
+ssh what2cook "tar -xzvf /var/www/html/build.tar.gz -C /var/www/html/"
+
+echo "${info}removing compressed files${default}"
+ssh what2cook "rm -f /var/www/html/build.tar.gz"
+rm -f build.tar.gz
 
 echo "${success}finished deploying build!${default}"
