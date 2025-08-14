d502ab723654e91227bedd147c3364cf611c4fc5
myFavBalu <skeyhl@gmail.com>
2025-08-14 22:04:16 +0200

    fixed db deployment script so that permissions are established correctly after deployment, fixed headerIssues for now

--- scripts/deploy_db.sh
+++ scripts/deploy_db.sh
@@ -6,4 +6,11 @@ echo "${info}copying database to server${default}"
 
 scp -r ../backend/var/data.db what2cook:/var/www/html/backend/var
 
+echo "${info}setting permissions for db-files${default}"
+
+ssh what2cook '
+chown -R www-data:www-data /var/www/html/backend/var/ &&
+find /var/www/html/backend/var/ -type f -exec chmod 660 {} \; &&
+find /var/www/html/backend/var/ -type d -exec chmod 770 {} \;'
+
 echo "${success}finished deploying database!${default}"
